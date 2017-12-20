38ca325ca9abb7688d5801268da8b1f35bcd7261
Drew Terry <nick.terry@gmail.com>
2017-12-20 12:32:26 -0500

    added comments for web.js disabled code

--- web.js
+++ web.js
@@ -15,5 +15,9 @@ console.log('Control Pin initialized.');
 
 app.listen(42713, () => console.log('Bed Warmer listening on 42713.'));
 
+//Removed close function until shut down routine is designed.  Shouldn't
+//be an issue because we only toggle high for 250 ms, but good practice 
+//anyways.
+
 //warmer.close(warmerParams);
 //console.log('Control Pin released.');
